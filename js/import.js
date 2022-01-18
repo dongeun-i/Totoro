a61d4d5843cf3ec1fa8238d9c936ea149fed8b62
DongEunLEE <dongeun1014@naver.com>
2022-01-18 16:33:18 +0900

    썸네일 이미지 클릭시 iframe 실행

--- js/import.js
+++ js/import.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     console.log($('section.story').eq(0).offset().top);
     console.log($(window).height()/2);
     var offset = $('body  div > fieldset figure').offset();
-    
+    // 스크롤 이벤트 
     $(document).scroll(function(){
     $('body > div fieldset section ol li').each(function(){
         $scroll = $(document).scrollTop();
@@ -21,7 +21,7 @@ $(document).ready(function(){
         }
     })
     });
-
+    // 접고펼치기
     $('.fold').click(function(){
       if(!$('fieldset span').hasClass('fixed')){
         $('fieldset span').addClass('fixed');
@@ -34,11 +34,11 @@ $(document).ready(function(){
         $('html,body').animate({scrollTop : 0}, 400);
       }
     })
-
+    // 상단으로 이동
     $('.top').click(function(){
       $('html,body').animate({scrollTop : 0}, 400);
     })
-
+    // 
     $('fieldset figure figcaption a:first-of-type').click(function(){
       if(!$(this).hasClass('hover')){
         $(this).addClass('hover')
@@ -80,7 +80,6 @@ $(document).ready(function(){
         $('html,body').animate({scrollTop : 1765}, 400);
       }
     })
-
     $('body header nav ul li.story').click(function(){
       if(!$('fieldset span').hasClass('fixed')){
         $("input:checkbox[name=fold]").prop("checked",true);
@@ -91,7 +90,25 @@ $(document).ready(function(){
         $('html,body').animate({scrollTop : 2702}, 400);
       }
     })
+    $('#start').on('change',function(){
+      player.playVideo()
+    })
+});
+  // 2. This code loads the IFrame Player API code asynchronously.
+  var tag = document.createElement('script');
 
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // 3. This function creates an <iframe> (and YouTube player)
+  //    after the API code downloads.
+  var player;
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      videoId: 'BJXMFkjhttI',
     });
+  }
+  var done = false;
 
 
