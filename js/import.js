d59092379660d13357ce008a2e72e0056c7845fc
DongEunLEE <dongeun1014@naver.com>
2022-01-19 16:44:06 +0900

    ftp 오류로인해 함수 위치만 바꿈

--- js/import.js
+++ js/import.js
@@ -100,6 +100,34 @@ $(document).ready(function(){
       player.playVideo();
     })
 });
+function isLike(){
+  let count = $('.count');
+  let num_count = Number(count.text())
+  if($('.like-btn').hasClass('is-active')){
+    $('.like-btn').removeClass('is-active');
+    num_count =num_count-1;
+    count.text(num_count)
+  }else{
+    $('.like-btn').addClass('is-active');
+    num_count =num_count+1;
+    count.text(num_count)
+  }
+}
+function copyUrl(){
+  var obShareUrl = $('<input>')
+  obShareUrl.value = window.document.location.href;  // 현재 URL 을 세팅해 줍니다.
+  obShareUrl.select();  // 해당 값이 선택되도록 select() 합니다
+
+  document.execCommand("copy"); // 클립보드에 복사합니다.
+
+  obShareUrl.blur(); // 선택된 것을 다시 선택안된것으로 바꿈니다.
+
+  alert("URL이 클립보드에 복사되었습니다"); 
+}
+
+
+
+
   // 2. This code loads the IFrame Player API code asynchronously.
   var tag = document.createElement('script');
 
@@ -127,27 +155,4 @@ $(document).ready(function(){
     }
     $('html,body').animate({scrollTop : targetOffsetTop}, 400);
   }
-  function isLike(){
-    let count = $('.count');
-    let num_count = Number(count.text())
-    if($('.like-btn').hasClass('is-active')){
-      $('.like-btn').removeClass('is-active');
-      num_count =num_count-1;
-      count.text(num_count)
-    }else{
-      $('.like-btn').addClass('is-active');
-      num_count =num_count+1;
-      count.text(num_count)
-    }
-  }
-  function copyUrl(){
-    var obShareUrl = $('<input>')
-    obShareUrl.value = window.document.location.href;  // 현재 URL 을 세팅해 줍니다.
-    obShareUrl.select();  // 해당 값이 선택되도록 select() 합니다
 
-    document.execCommand("copy"); // 클립보드에 복사합니다.
-
-    obShareUrl.blur(); // 선택된 것을 다시 선택안된것으로 바꿈니다.
-
-    alert("URL이 클립보드에 복사되었습니다"); 
-  }
